ceaf910d5efa81283f3ba5f60700f3c128ba2ce5
derek <derekgong.dg@gmail.com>
2016-12-10 16:45:58 +0800

    FaceRepalcer

---
+++ EmojiReplacer/EmojiReplacer/FaceReplacer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using Microsoft.ProjectOxford.Vision;
+using VideoFrameAnalyzer;
+
+namespace EmojiReplacer
+{
+    class FaceReplacer
+    {
+
+    }
+}
